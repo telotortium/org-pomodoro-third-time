2f986a7a5eee5f72b01e816d3023e796fd015da4
robert.irelan <robert.irelan@bytedance.com>
2022-02-08 21:29:34 -0800

    Remove message calls

--- org-pomodoro-third-time.el
+++ org-pomodoro-third-time.el
@@ -185,9 +185,6 @@ Format: value returned by ‘encode-time’.")
   "Update the bank if the break is shorter or longer than calculated.
 Argument FN contains the original function advised by this.
 Argument STATE contains the STATE argument passed to ‘org-pomodoro-start’."
-  (message "Expected break time %S"
-           org-pomodoro-third-time--expected-break-time)
-  (message "bank seconds %S" org-pomodoro-third-time--bank-seconds)
   (when (and (or (null state)
                  (eq state :pomodoro))
              org-pomodoro-third-time--expected-break-time)
@@ -197,9 +194,12 @@ Argument STATE contains the STATE argument passed to ‘org-pomodoro-start’."
                             org-pomodoro-third-time--start-time)))
            (delta
             (- org-pomodoro-third-time--expected-break-time actual-break-time)))
+      (message "actual-break-time %S delta %S" actual-break-time delta)
       (setq org-pomodoro-third-time--bank-seconds
             (+ org-pomodoro-third-time--bank-seconds delta))
+      (message "bank seconds %S" org-pomodoro-third-time--bank-seconds)
       (setq org-pomodoro-third-time--expected-break-time nil)))
+  (message "bank seconds %S" org-pomodoro-third-time--bank-seconds)
   (funcall fn state))
 
 (defun org-pomodoro-third-time--reset-bank ()
